7d5a1ddd49c83b68b6f93073623214ea4d823ad1
sonalichaudhary844@gmail.com <sonalichaudhary884>
2024-11-25 23:47:40 +0530

    prplem

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,11 @@
 import Navbar from "./Navbar";
+import "./index.css";
 
 const Home = () => {
   return (
     <>
-      <Navbar />
-      <div className="container">
-        <img
-          src="https://aakash-sharma.netlify.app/static/media/cloudBg.f28b5c6961fd82be839c.png"
-          alt="image"
-        />
+      <div className="containerhome">
+        <Navbar />
 
         <div className="left">
           <h1>sonali is very beautiful</h1>
