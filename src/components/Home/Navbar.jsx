02b3b2f0aff8ce52103ab8a72cbe0c5f8455950d
sonalichaudhary844@gmail.com <sonalichaudhary884>
2024-11-25 23:14:52 +0530

    background img

---
+++ src/components/Home/Navbar.jsx
@@ -0,0 +1,28 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>sona</div>
+
+      <div>
+        <ul id="navbar">
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Project</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Home/index.js
@@ -0,0 +1,27 @@
+import Navbar from "./Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <img
+          src="https://aakash-sharma.netlify.app/static/media/cloudBg.f28b5c6961fd82be839c.png"
+          alt="image"
+        />
+
+        <div className="left">
+          <h1>i am sonali</h1>
+          <a href="" className="btn btn-outline-warning">
+            <button>Resume</button>
+          </a>
+        </div>
+        <div className="right">
+          <img src="" alt="image not found" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
