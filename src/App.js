ab50b473925bbc0092568a09432a58f2acccf6bc
sonalichaudhary844@gmail.com <sonalichaudhary884>
2024-11-30 01:02:39 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import About from "./components/About/About";
 import Home from "./components/Home";
 
 function App() {
   return (
     <div>
       <Home />
+      <About />
+      <SkillContainer />
     </div>
   );
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,13 +7,19 @@ const Home = () => {
       <div className="containerhome">
         <Navbar />
         <div className="containerimg">
-          <div className="left">
-            <h1>
-              <span>hi i am sonali </span>
-              <span> i am front-end developer</span>
+          <div class="sm:text-center lg:text-left">
+            <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+              <span class="block text-black">hi i am sonali </span>
+              <span class="block text-blue-500 z-0 lg:inline">
+                {" "}
+                i am front-end developer
+              </span>
             </h1>
-            <p> i am working i am front-end full stack developer</p>
-            <div className="logo">
+            <p class="mt-3 text-base text-black sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+              {" "}
+              i am working i am front-end full stack developer
+            </p>
+            <div class=" flex md:justify-start">
               <a href="" class="mr-5 cursor-pointer mt-8 hover:scale-125">
                 <img src="https://img.icons8.com/doodle/40/000000/linkedin--v2.png" />
               </a>
@@ -27,19 +33,19 @@ const Home = () => {
                 <img src="https://img.icons8.com/doodle/40/000000/instagram-new--v2.png" />
               </a>
             </div>
-            <div>
-              {" "}
-              <a href="" className="btn btn-outline-warning">
-                <button>Resume</button>
-              </a>
+            <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg-justify-start">
+              <div class="mt-3 sm:mt-0 cursor-pointer w-1/2">
+                <a class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10">
+                  Resume
+                </a>
+              </div>
             </div>
           </div>
-          <div className="right">
-            <img
-              src="https://aakash-sharma.netlify.app/static/media/webdev.4d72dbba32efee3890cef9bcacce7aa7.svg"
-              alt=""
-            />
-          </div>
+          <img
+            src="https://aakash-sharma.netlify.app/static/media/webdev.4d72dbba32efee3890cef9bcacce7aa7.svg"
+            alt
+            class="md:w-3/6 hidden sm:block"
+          />
         </div>
       </div>
     </>
